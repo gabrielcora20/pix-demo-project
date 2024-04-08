25cc7cdffe0cf737441e287cbc82855173e0a047
Gabriel Corá <gabrielcora20@gmail.com>
2024-04-08 13:39:24 -0300

    feat: adding dashboards

--- source/listener/src/main/kotlin/com/gabrielcora/listener/dto/PaymentEventResponseDto.kt
+++ source/listener/src/main/kotlin/com/gabrielcora/listener/dto/PaymentEventResponseDto.kt
@@ -1,10 +1,11 @@
 package com.gabrielcora.listener.dto
 
 import com.gabrielcora.pix.payment.domain.models.enums.StatusEnum
+import org.bson.types.ObjectId
 import java.time.LocalDateTime
 
 data class PaymentEventResponseDto(
-    val id: String,
+    val id: ObjectId,
     val status: StatusEnum,
     val inclusionDate: LocalDateTime,
     val paymentDate: LocalDateTime?,
